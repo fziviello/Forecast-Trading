8bb98da4e303da0cdf9f8bae532657de1caafec6
Fabio Ziviello <f.ziviello@gmail.com>
2024-11-18 07:44:30 +0100

    updated training for send notify

--- scripts/training.py
+++ scripts/training.py
@@ -8,11 +8,15 @@ from datetime import datetime
 from folder_config import setup_folders, LOGS_FOLDER, LOG_TRAINING_FILE_PATH
 from config import TIME_MINUTE_REPEAT, N_REPEAT
 
+SEND_TELEGRAM = False
+
 setup_folders()
 
 logging.basicConfig(filename=os.path.join(LOGS_FOLDER, LOG_TRAINING_FILE_PATH), level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def run_scripts_for_symbol(symbol):
+    global SEND_TELEGRAM
+    
     print(f"\033[93m*** Avvio Creazione del DataSet per {symbol}\033[0m\n")
     process1 = subprocess.Popen(
         ["python", "create_dataSet.py", "--symbol", symbol],
@@ -34,8 +38,11 @@ def run_scripts_for_symbol(symbol):
     process1.wait()
 
     print(f"\n\033[93m*** Avvio Forecast per {symbol}\033[0m\n")
+    command = ["python", "forecast_bot.py", "--symbol", symbol]
+    command.extend(["--notify", str(SEND_TELEGRAM).upper()])
+
     process2 = subprocess.Popen(
-        ["python", "forecast_bot.py", "--symbol", symbol],
+        command,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         bufsize=1,
@@ -80,8 +87,12 @@ def schedule_scripts(symbols, N_REPEAT):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Scheduler per esecuzione script multipli.")
     parser.add_argument("--symbols", type=str, required=True, help="Lista di simboli separati da virgola (es: EURUSD,AUDJPY,GBPUSD)")
+    parser.add_argument("--notify", type=bool, required=False, help="Invia notifica al canale telegram")
     args = parser.parse_args()
-
+    
+    if args.notify is not None :
+        SEND_TELEGRAM = args.notify
+    
     symbols = (args.symbols.upper()).split(',')
     now = datetime.now()
     print(f'\033[96mTrainer Avviato il {now.strftime("%Y-%m-%d %H:%M:%S")} per {N_REPEAT} esecuzioni\033[0m\n')

8d51ca5c76a20120840827d5c47a96f6492624b7
Fabio Ziviello <f.ziviello@gmail.com>
2024-11-18 17:53:40 +0100

    update

--- scripts/config.py
+++ scripts/config.py
@@ -11,7 +11,7 @@ INTERVAL = '2m'
 FORECAST_VALIDITY_MINUTES = 30
 TIME_MINUTE_REPEAT = 5
 N_REPEAT = 60
-BOT_TOKEN = "7439330565:AAFNP8E91iIaHfbs-F0apaYXrMCMz2hivqQ"
+BOT_TOKEN = ""
 CHANNEL_TELEGRAM = "@SignalFaz"
 
 PARAM_GRID = {
@@ -22,10 +22,3 @@ PARAM_GRID = {
     'learning_rate': [0.001, 0.005, 0.01],
     'optimizer': ['adam', 'rmsprop']
 }
-
-#Tempo di elaborazione migliore configurazione per AUDJPY: 13:21:49
-#Migliori parametri trovati per AUDJPY: {'units': 75, 'dropout': 0.2, 'epochs': 50, 'batch_size': 32, 'learning_rate': 0.001, 'optimizer': 'rmsprop'} con accuracy=1.000%à#
-#Migliori parametri trovati per AUDJPY: {'units': 50, 'dropout': 0.2, 'epochs': 50, 'batch_size': 16, 'learning_rate': 0.01, 'optimizer': 'adam'} con accuracy=0.700%
-
-#Tempo di elaborazione migliore configurazione per AUDNZD: 15:15:36
-#Migliori parametri trovati per AUDNZD: {'units': 50, 'dropout': 0.2, 'epochs': 50, 'batch_size': 16, 'learning_rate': 0.001, 'optimizer': 'adam'} con accuracy=0.700%
